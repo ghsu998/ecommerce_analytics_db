8174d8e96429aaef9cf78a2f84b4aedf8136f3f3
Gary Hsu <51069002+ghsu998@users.noreply.github.com>
2025-03-31 00:27:12 -0700

    tyro v2

--- tyro_gateway/models/api_trigger.py
+++ tyro_gateway/models/api_trigger.py
@@ -1,12 +1,10 @@
 from pydantic import BaseModel
-from typing import Optional
-from datetime import date
 from datetime import datetime
 
 class APITrigger(BaseModel):
-    timestamp: datetime
-    trigger_source: str
-    status: str
-    data_summary: str
-    endpoint: str
     action_name: str
+    endpoint: str
+    data_summary: str
+    trigger_source: str
+    timestamp: datetime  # ⬅️ 確保是 datetime，不是 date
+    status: str

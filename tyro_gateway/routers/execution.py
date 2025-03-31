e146e50e91756ff45f760bc97b4f4c455a77b7d2
Gary Hsu <51069002+ghsu998@users.noreply.github.com>
2025-03-30 20:55:20 -0700

    TYRO v2 update

--- tyro_gateway/routers/execution.py
+++ tyro_gateway/routers/execution.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
 from tyro_gateway.notion_client import create_record
-from tyro_gateway.models.api_trigger import ApiTriggerLog
+from tyro_gateway.models.api_trigger import ApiTrigger
 
 router = APIRouter()
 
 @router.post("/add-api-trigger-log")
-def add_api_trigger(data: ApiTriggerLog):
+def add_api_trigger(data: ApiTrigger):
     return create_record("5.1", data.dict())

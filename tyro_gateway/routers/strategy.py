63457888b7ea18e23a4da1e14699d1ba6e6f65c5
Gary Hsu <51069002+ghsu998@users.noreply.github.com>
2025-03-30 21:01:34 -0700

    TYRO v2 update

--- tyro_gateway/routers/strategy.py
+++ tyro_gateway/routers/strategy.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
 from tyro_gateway.notion_client import create_record
-from tyro_gateway.models.strategy import StrategyEntry
+from tyro_gateway.models.strategy import Strategy
 
 router = APIRouter()
 
 @router.post("/add-strategy")
-def add_strategy(data: StrategyEntry):
+def add_strategy(data: Strategy):
     return create_record("6.1", data.dict())

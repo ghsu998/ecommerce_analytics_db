62d0f792ed7400aafcd39c487d3ed61e489274ac
Gary Hsu <51069002+ghsu998@users.noreply.github.com>
2025-04-13 14:59:28 -0700

    tyro gateway update

--- tyro_gateway/models/resume_version.py
+++ tyro_gateway/models/resume_version.py
@@ -10,3 +10,4 @@ class ResumeVersion(BaseModel):
     target_job_title: str = Field(..., description="履歷目標職位")
     resume_summary: str = Field(..., description="摘要簡介內容")
     cover_letter_content: str = Field(default="", description="Cover Letter 的內文")
+    unique_key: str = Field(..., description="唯一識別碼，建議使用 target_job_title + date_created")
